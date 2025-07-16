55107fb205ef99c54b6e238f7b75eeb7e75e3431
TranBin848 <tranbinn848@gmail.com>
2025-07-16 20:18:43 +0700

    Add walk and idle animation for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // tốc độ di chuyển
-
-    private Rigidbody2D rb;
     private Vector2 movement;
 
+    private Rigidbody2D rb;
+    private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,12 +21,23 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true); // Cập nhật trạng thái hoạt hình
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false); // Dừng hoạt hình khi không có di chuyển
+            animator.SetFloat("LastInputX", movement.x);
+            animator.SetFloat("LastInputY", movement.y);
+        }
+
         // Lấy giá trị di chuyển từ Input System
         movement = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", movement.x); // Cập nhật giá trị di chuyển theo trục X
+        animator.SetFloat("InputY", movement.y); // Cập nhật giá trị di chuyển theo trục Y
+
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = movement * moveSpeed; // Cập nhật vận tốc của Rigidbody2D
+        rb.linearVelocity = movement * moveSpeed; // Cập nhật vận tốc của Rigidbody2D 
     }
 }
