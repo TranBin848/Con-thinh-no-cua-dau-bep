5852e3329d8b425486e6e3b32a67fe8b2ce31c4d
TranBin848 <tranbinn848@gmail.com>
2025-07-16 19:35:54 +0700

    Add player sprites and basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // tốc độ di chuyển
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        // Lấy giá trị di chuyển từ Input System
+        movement = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * moveSpeed; // Cập nhật vận tốc của Rigidbody2D
+    }
+}
