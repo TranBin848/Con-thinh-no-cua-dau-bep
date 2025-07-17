ff1d8f22d9dfe791d8deb1c40747ddb4e7283c01
TranBin848 <tranbinn848@gmail.com>
2025-07-18 00:44:41 +0700

    Add animated door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    private Animator animator; 
+    private bool isOpen = false; 
+    private bool playerInTrigger = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public bool canInteract()
+    {
+        return playerInTrigger && !isOpen; // Chỉ cho phép tương tác nếu cửa chưa mở
+    }
+
+    public void Interact()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            animator.SetTrigger("OpenDoor"); // Trigger animation mở cửa
+                                             // spriteRenderer.sprite = openSprite; // Nếu dùng sprite
+            Debug.Log("Door opened!");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+            if (canInteract())
+            {
+                Interact(); // Tự động mở cửa khi chạm
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+            isOpen = false;
+            animator.SetTrigger("CloseDoor"); // Trigger animation đóng cửa
+        }
+    }
+}
